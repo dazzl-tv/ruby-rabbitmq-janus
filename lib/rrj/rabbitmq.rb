eb9949e749e2e986cd817362e8018f0795417d80
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-16 17:51:04 +0200

    Complete connection

--- lib/rrj/rabbitmq.rb
+++ lib/rrj/rabbitmq.rb
@@ -11,8 +11,20 @@ module RRJ
       @settings = configuration
       @logs = logs
 
+      start_communication
+    end
+
+    private
+
+    def start_communication
       @logs.write 'Prepare connection with RabbitMQ server'
       @logs.write @settings.inspect
+
+      @connection = Bunny.new
+      @connection.start
+      @channel = @connection.create_channel
+      @queue = @channel.queue('from-janus')
+      @connection.close
     end
   end
 end

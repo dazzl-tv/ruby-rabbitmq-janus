da281059643240174f2aff0e24425554ffffdc70
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-16 18:18:20 +0200

    Fix options to rabbitmq server

--- lib/rrj/rabbitmq.rb
+++ lib/rrj/rabbitmq.rb
@@ -4,7 +4,7 @@ require 'bunny'
 
 module RRJ
   # Class for connection with RabbitMQ Server
-  class Rabbit
+  class RabbitMQ
     attr_reader :connection, :logs, :settings
 
     def initialize(configuration, logs)
@@ -17,14 +17,20 @@ module RRJ
     private
 
     def start_communication
-      @logs.write 'Prepare connection with RabbitMQ server'
-      @logs.write @settings.inspect
-
-      @connection = Bunny.new
+      @connection = Bunny.new(read_options_server)
       @connection.start
       @channel = @connection.create_channel
       @queue = @channel.queue('from-janus')
       @connection.close
+    end
+
+    def read_options_server
+      hash = {}
+      @logs.write 'Prepare connection with RabbitMQ server : '
+      @settings.options.fetch('server').each do |key, server|
+        @logs.write '[' + key.to_s + '] ' + server.to_s
+        hash.merge!(key.to_sym => server.to_s)
+      end
     end
   end
 end

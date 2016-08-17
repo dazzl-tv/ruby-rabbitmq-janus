3eb5a8d28cd87568af8aee68eda0f1c8fcaa4466
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-17 13:12:12 +0200

    Describ a constant for doc

--- lib/rrj/version.rb
+++ lib/rrj/version.rb
@@ -2,8 +2,13 @@
 # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
 # Define constant to gem.
 module RRJ
+  # Define version to gem
   VERSION = '0.1.0'
+
+  # Define a summary description to gem
   SUMMARY = 'Ruby RabbitMQ Janus'
+
+  # Define a long description to gem
   DESCRIPTION = 'This gem is used to communicate to a server Janus through RabbitMQ '\
     'software (Message-oriented middleware). It waiting a messages to Rails API who ' \
     'send to RabbitMQ server in a queue for janus server. Janus processes a message ' \

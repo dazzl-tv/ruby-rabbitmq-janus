cfb27297fec417c030fb1e92d337a9822d5afba6
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-17 16:55:47 +0200

    Replace default spec

--- spec/rrj/rrj_spec.rb
+++ spec/rrj/rrj_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require 'spec_helper'
 
-describe Gem::Janus do
+describe RRJ do
   it 'has a version number' do
-    expect(Gem::Janus::VERSION).not_to be nil
+    expect(RRJ::VERSION).not_to be nil
   end
 
   it 'does something useful' do

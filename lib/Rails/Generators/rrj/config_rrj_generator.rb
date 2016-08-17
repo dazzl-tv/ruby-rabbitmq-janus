6b83e92cb161e223ac1c94841adbb5b8ec3ca3f0
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-17 16:44:09 +0200

    Rename

---
+++ lib/Rails/Generators/rrj/config_rrj_generator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails/generators'
+
+# @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+# Generate a file for this gem
+class ConfigRrjGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../../../config', __FILE__)
+
+  desc 'This generator create a default config at config/ruby-rabbitmq-janus.yml'
+  def create_config_file
+    create_file 'config/ruby-rabbitmq-janus.yml', '# Add configuration to gem here'
+  end
+end

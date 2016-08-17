2cdcabbe9624e19857ed374e03d4e197263c7c1a
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-17 13:19:49 +0200

    Add doc

--- lib/rrj/config.rb
+++ lib/rrj/config.rb
@@ -4,12 +4,22 @@ require 'yaml'
 require 'logging'
 
 module RRJ
-  # Configuration of gem
+  # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+  # Loading a yaml file for apply a configuration to gem.
+  # @!attribute [r] options
+  #   @return  [RRJ::Config] Options to gem.
+  # @!attribute [r] logs
+  #   @return [RRJ::Log] Object instance to logs gem.
   class Config
     attr_reader :options, :logs
 
+    # Define a default path to file configuration
     DEFAULT_PATH = File.realpath(File.join(File.dirname(__FILE__), '..', '..'))
+
+    # Define a default name to file configuration
     DEFAULT_CONF = File.join(DEFAULT_PATH, 'config', 'default.yml')
+
+    # Define a default override file configuration
     CUSTOMIZE_CONF = '.rrj.yml'
 
     def initialize(logs)

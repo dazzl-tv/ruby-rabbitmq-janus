6d3b52dbe8f9545667326388c74583e4c526b3f7
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-18 15:47:27 +0200

    Cleanning log

--- lib/rrj/log.rb
+++ lib/rrj/log.rb
@@ -12,36 +12,32 @@ module RRJ
     # Define file name to writing logs
     DEFAULT_LOG_NAME = 'rails-rabbit-janus.log'
 
-    # Define head for each line in log
-    DEFAULT_HEAD_LOG = '[RRJ]'
-
-    # Define a default level to gem
+    # Define a default level to gem (:debug :info :warn :error :fatal :unknow)
     DEFAULT_LEVEL = :info
 
     # Returns a new instance to Log
     def initialize
-      @logs = Logging.logger[DEFAULT_HEAD_LOG]
+      @logs = Logging.logger[Time.now.to_s + ' --']
       @logs.level = DEFAULT_LEVEL
       @logs.add_appenders Logging.appenders.file(DEFAULT_LOG_DIR + '/' + DEFAULT_LOG_NAME)
 
-      @logs.write('====================================')
-      @logs.write("\n")
-      @logs.write('### Start gem Rails Rabbit Janus ###')
-      @logs.write("\n")
-      @logs.write('====================================')
-      @logs.write("\n")
+      welcome = \
+        "====================================\n" \
+        "### Start gem Rails Rabbit Janus ###\n" \
+        "====================================\n"
+
+      @logs.write(welcome)
     end
 
     # Write a message in log file.
     # @param message [String] Message writing in logs
     # @param level [Symbol] Level to message (:info, :warn, :debug)
     def write(message, level = DEFAULT_LEVEL)
-      msg = Time.now.strftime('%Y/%m/%d %H:%M:%S') + ' : ' + message
       case level
       when :info
-        @logs.info msg
+        @logs.info message
       when :warn
-        @logs.warn msg
+        @logs.warn message
       end
     end
   end

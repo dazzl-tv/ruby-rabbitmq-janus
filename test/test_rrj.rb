c8cd090e4672a834f9f2376088f404d330303b61
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-19 10:39:04 +0200

    Delete old test

--- test/test_rrj.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'minitest/autorun'
-require 'rrj'
-
-class RRJTest < Minitest::Test
-  def test_say_hello
-    assert_equal 'Yopi yop', RRJ::RRJ.new
-  end
-end

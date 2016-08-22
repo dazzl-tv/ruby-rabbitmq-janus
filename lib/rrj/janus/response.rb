c2f0623f737acac870d7dfd14ea3271eb5503033
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-22 18:16:34 +0200

    Refractoring

--- lib/rrj/janus/response.rb
+++ lib/rrj/janus/response.rb
@@ -6,6 +6,7 @@ module RRJ
   # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
   # Response Janus received to RabbitMQ server
   class ResponseJanus
+    attr_reader :response
     def initialize(opts = {})
       @correlation_id = opts[:correlation_id]
       @transaction = opts[:transaction]
@@ -14,22 +15,21 @@ module RRJ
 
     # Read a response to janus (in RabbitMQ queue)
     def read(channel)
-      body = nil
-      the_queue = channel.queue(@reply_queue_name)
+      @channel = channel
+      the_queue = @channel.queue(@reply_queue_name)
       the_queue.subscribe(block: true, manual_ack: true) do |info, prop, pay|
-        body = listen info, prop, pay
+        listen(info, prop, pay)
       end
-      body
+      @response
     end
 
     private
 
-    def listen(_delivery_info, properties, payload)
+    def listen(delivery_info, properties, payload)
       if @correlation_id == properties[:correlation_id]
-        body = payload
-        channel.consumers[delivery_info.consumer_tag].cancel
+        @response = payload
+        @channel.consumers[delivery_info.consumer_tag].cancel
       end
-      body
     end
   end
 end

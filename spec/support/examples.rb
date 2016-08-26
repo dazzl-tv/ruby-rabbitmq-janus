82c7fd04f27c869c99ed4c821dc8acd9631442dc
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-26 16:57:09 +0200

    Delete unless shared example

--- spec/support/examples.rb
+++ spec/support/examples.rb
@@ -5,9 +5,3 @@ shared_examples 'file is found' do
     expect(file?(file)).to be true
   end
 end
-
-shared_examples 'send a request' do
-  it 'should a Hash' do
-    expect(transaction.sending_a_message_info.class).to eq Hash
-  end
-end

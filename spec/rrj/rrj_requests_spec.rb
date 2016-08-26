6987e57d06534c5af32b1a2440556e26b33afa5b
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-26 16:56:17 +0200

    Fix test request sending$

--- spec/rrj/rrj_requests_spec.rb
+++ spec/rrj/rrj_requests_spec.rb
@@ -3,12 +3,20 @@
 require 'spec_helper'
 
 describe 'RRJ::RRJ' do
-  before(:context) do
-    @transaction = RRJ::RRJ.new
-  end
-
   describe '.sending_a_message' do
-    it { expect(@transaction.sending_a_message_info.class).to eq Hash }
-    it { expect(@transaction.sending_a_message_create.class).to eq Hash }
+    let(:transaction) { RRJ::RRJ.new }
+
+    it 'send a request info' do
+      expect(transaction.sending_a_message_info.class).to eq Hash
+    end
+
+    it 'send a request create' do
+      expect(transaction.sending_a_message_create.class).to eq Hash
+    end
+
+    let(:create) { transaction.sending_a_message_create }
+    it 'send a request destroy' do
+      expect(transaction.sending_a_message_destroy(create).class).to eq Hash
+    end
   end
 end

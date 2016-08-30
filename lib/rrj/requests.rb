0dc2faade8ef3261d5961d81f2f8e77ad890046e
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-30 15:50:02 +0200

    Read subfolder

--- lib/rrj/requests.rb
+++ lib/rrj/requests.rb
@@ -19,10 +19,42 @@ module RRJ
 
       @logs.info "Loading all requests in : #{PATH_REQUEST}"
 
-      Dir[File.join(PATH_REQUEST, '**', '*')].count do |file|
-        @requests[File.basename(file, '.json').to_sym] = File.path(file)
+      each_files
+
+      @requests.each do |key, value|
+        @logs.debug "[#{key}] #{value}"
       end
       @requests
+    end
+
+    private
+
+    def each_files
+      Dir[File.join(PATH_REQUEST, '*')].count do |file|
+        if File.file?(file)
+          read_file(file)
+        elsif File.directory?(file)
+          each_folder(File.basename(file))
+        end
+      end
+    end
+
+    def each_folder(subfolder)
+      Dir[File.join(PATH_REQUEST + subfolder, '*')].count do |file|
+        if File.file?(file)
+          read_folder(subfolder.gsub('/', '::') + '::', file)
+        elsif File.directory?(file)
+          each_folder(subfolder + '/' + File.basename(file))
+        end
+      end
+    end
+
+    def read_file(file)
+      @requests[File.basename(file, '.json').to_s] = File.path(file)
+    end
+
+    def read_folder(folder, file)
+      @requests[folder + File.basename(file, '.json').to_s] = File.path(file)
     end
   end
 end

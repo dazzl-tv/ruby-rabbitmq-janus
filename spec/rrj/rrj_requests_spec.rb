c96a87947e3768b0d1f6fa236908080ba3538430
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-31 18:02:37 +0200

    Fix test destroy and replace methods with aliases

--- spec/rrj/rrj_requests_spec.rb
+++ spec/rrj/rrj_requests_spec.rb
@@ -6,24 +6,26 @@ describe 'RRJ::RRJ' do
   describe '.message_template_ask' do
     let(:transaction) { RRJ::RRJ.new }
 
-    let(:request_info) { transaction.message_template_ask }
+    let(:request_info) { transaction.ask }
     it 'send a request info' do
       expect(request_info.class).to eq Hash
     end
 
-    let(:response_info) { transaction.message_template_response(request_info) }
+    let(:response_info) { transaction.response(request_info) }
     it 'ask a response to info request' do
       expect(response_info.class).to eq Hash
     end
 
-    let(:create) { transaction.message_template_ask('create') }
+    let(:create) { transaction.ask('create') }
     it 'send a request create' do
       expect(create.class).to eq Hash
     end
 
+    let(:create) { transaction.ask('create') }
+    let(:create_response) { transaction.response(create) }
+    let(:destroy) { transaction.ask('destroy', create_response) }
     it 'send a request destroy' do
-      expect(transaction.message_template_ask('destroy').class).to eq Hash
-      expect(transaction.message_template_response(create).class).to eq Hash
+      expect(transaction.response(destroy).class).to eq Hash
     end
   end
 end

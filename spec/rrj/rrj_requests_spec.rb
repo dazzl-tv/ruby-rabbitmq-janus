b86b4c185fb729484b46815afd98afad824f4dcf
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-08-31 15:51:23 +0200

    Complete spec

--- spec/rrj/rrj_requests_spec.rb
+++ spec/rrj/rrj_requests_spec.rb
@@ -6,16 +6,23 @@ describe 'RRJ::RRJ' do
   describe '.message_template_ask' do
     let(:transaction) { RRJ::RRJ.new }
 
+    let(:request_info) { transaction.message_template_ask }
     it 'send a request info' do
-      expect(transaction.message_template_ask.class).to eq Hash
+      expect(request_info.class).to eq Hash
     end
 
-    it 'send a request create' do
-      expect(transaction.message_template_ask('create').class).to eq Hash
+    let(:response_info) { transaction.message_template_response(request_info) }
+    it 'ask a response to info request' do
+      expect(response_info.class).to eq Hash
     end
 
     let(:create) { transaction.message_template_ask('create') }
+    it 'send a request create' do
+      expect(create.class).to eq Hash
+    end
+
     it 'send a request destroy' do
+      expect(transaction.message_template_ask('destroy').class).to eq Hash
       expect(transaction.message_template_response(create).class).to eq Hash
     end
   end

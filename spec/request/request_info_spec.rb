7e9a99aa109c9260748bba1d95d6b58c204d030d
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-09-06 11:11:28 +0200

    Fix info request validation

--- spec/request/request_info_spec.rb
+++ spec/request/request_info_spec.rb
@@ -7,12 +7,8 @@ describe 'RRJ::RRJ' do
   let(:info_request) { transaction.ask }
   let(:info_response) { transaction.response(info_request) }
 
-  let(:destroy_request) { transaction.ask('destroy', info_response) }
-  let(:destroy_response) { transaction.response(destroy_request) }
-
-  describe '.response' do
+  describe '.response', type: :info do
     it_behaves_like 'request simple', :info do
-      let(:request) { info_request }
       let(:response) { info_response }
     end
   end

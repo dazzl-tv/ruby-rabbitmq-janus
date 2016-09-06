80a2d26d0b5fa8f22f9bfc674df3e2dc48ee0b27
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-09-06 14:25:46 +0200

    Fix test/validator channel list

--- spec/request/request_channel_spec.rb
+++ spec/request/request_channel_spec.rb
@@ -3,34 +3,35 @@
 require 'spec_helper'
 
 describe 'RRJ::RRJ' do
-  # Request type info
-  let(:info_request) { transaction.ask }
-  let(:info_response) { transaction.response(info_request) }
+  describe '.response', type: :channel_list do
+    let(:transaction) { RRJ::RRJ.new }
 
-  # Request type create
-  let(:create_request) { transaction.ask('create') }
-  let(:create_response) { transaction.response(create_request) }
+    # Request type create
+    let(:create_request) { transaction.ask('create') }
+    let(:create_response) { transaction.response(create_request) }
 
-  # Request type attach
-  let(:attach_request) { transaction.ask('attach', create_response) }
-  let(:attach_response) { transaction.response(attach_request) }
+    # Request type attach
+    let(:attach_request) { transaction.ask('attach', create_response) }
+    let(:attach_response) { transaction.response(attach_request) }
 
-  # Request type channel::list
-  let(:channel_list_request) { transaction.ask('channel::list', attach_response) }
-  let(:channel_list_response) { transaction.response(channel_list_request) }
+    # Request type channel::list
+    let(:channel_list_request) { transaction.ask('channel::list', attach_response) }
+    let(:channel_list_response) { transaction.response(channel_list_request) }
 
-  # Request type detach
-  let(:detach_request) { transaction.ask('detach', attach_response) }
-  let(:detach_response) { transaction.response(detach_request) }
+    # Request type detach
+    let(:detach_request) { transaction.ask('detach', attach_response) }
+    let(:detach_response) { transaction.response(detach_request) }
 
-  # Request type destroy
-  let(:destroy_request) { transaction.ask('destroy', detach_response) }
-  let(:destroy_response) { transaction.response(destroy_request) }
+    # Request type destroy
+    let(:destroy_request) { transaction.ask('destroy', detach_response) }
+    let(:destroy_response) { transaction.response(destroy_request) }
 
-  describe '.response' do
-    it_behaves_like 'request channel', 'channel::list', 'channel::list' do
-      let(:request) { channel_list_request }
-      let(:response) { channel_list_response }
+    it 'type channel::list' do
+      expect(create_response).to match_json_schema(:create)
+      expect(attach_response).to match_json_schema(:attach)
+      expect(channel_list_response).to match_json_schema(:channel_list)
+      expect(detach_response).to match_json_schema(:detach)
+      expect(destroy_response).to match_json_schema(:destroy)
     end
   end
 end

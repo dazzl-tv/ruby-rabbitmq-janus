add6d0e64c6ac563fa6403e6e02a7a430fffa339
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-09-23 11:26:12 +0200

    Add test for async request

---
+++ spec/request/request_info_async_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'RubyRabbitmqJanus::RRJ' do
+  describe '.response', type: :request_async, name: :info do
+    let(:transaction) { RubyRabbitmqJanus::RRJ.new }
+
+    # Request type info
+    let(:info) { transaction.ask_async }
+
+    it 'type info' do
+      expect(info).to match_json_schema(:info)
+    end
+  end
+end

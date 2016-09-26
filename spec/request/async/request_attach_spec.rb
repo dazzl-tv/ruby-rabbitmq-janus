6cb240be65bc15b7285d7912f564b4822fdb7428
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-09-26 11:14:17 +0200

    Complete requests test

--- spec/request/async/request_attach_spec.rb
+++ spec/request/async/request_attach_spec.rb
@@ -3,17 +3,21 @@
 require 'spec_helper'
 
 describe 'RubyRabbitmqJanus::RRJ' do
-  describe '.response', type: :request_async, name: :create do
+  describe '.response', type: :request_async, name: :attach do
     let(:transaction) { RubyRabbitmqJanus::RRJ.new }
 
     # Request type create
     let(:create) { transaction.ask_async('create') }
 
-    # Request type destroy
-    let(:destroy) { transaction.ask_async('destroy', create) }
+    # Request type attach
+    let(:attach) { transaction.ask_async('attach', create) }
 
-    it 'type create ASYNC' do
+    # Request type destroy
+    let(:destroy) { transaction.ask_async('destroy', attach) }
+
+    it 'type attach ASYNC' do
       expect(create).to match_json_schema(:create)
+      expect(attach).to match_json_schema(:attach)
       expect(destroy).to match_json_schema(:destroy)
     end
   end

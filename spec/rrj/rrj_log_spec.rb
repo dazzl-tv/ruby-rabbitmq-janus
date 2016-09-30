69c5dfb32d4ee07d5bdfca85a1caaa8b26335c28
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-09-30 15:48:54 +0200

    Delete spec unless

--- spec/rrj/rrj_log_spec.rb
+++ spec/rrj/rrj_log_spec.rb
@@ -3,18 +3,6 @@
 require 'spec_helper'
 
 describe 'RubyRabbitmqJanus::Log' do
-  it 'Has a default log folder' do
-    expect(RubyRabbitmqJanus::Log::DEFAULT_LOG_DIR).to eq 'log'
-  end
-
-  it 'Has a default log name' do
-    expect(RubyRabbitmqJanus::Log::DEFAULT_LOG_NAME).to eq 'rails-rabbit-janus.log'
-  end
-
-  it 'Has a default level' do
-    expect(RubyRabbitmqJanus::Log::DEFAULT_LEVEL).to eq :debug
-  end
-
   it 'Create an instance of RubyRabbitmqJanus::Log' do
     expect(RubyRabbitmqJanus::Log.instance).not_to be nil
   end

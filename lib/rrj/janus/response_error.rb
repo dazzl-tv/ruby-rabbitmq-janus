a45c28134a4d0ed74dfb2504896980e10eea2f96
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-03 16:46:13 +0200

    Add class for test response janus is an error

---
+++ lib/rrj/janus/response_error.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'bunny'
+
+module RubyRabbitmqJanus
+  # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+  # Class for test if response return an janus error
+  class ResponseError
+    # Return an Hash to request
+    def initialize(request)
+      @request = Has.new(request)
+    end
+
+    # Test if response is an error
+    def test_request_return
+      @request['janus'] == 'error' ? true : false
+    end
+
+    # Return a reaon to error returning by janus
+    def reason
+      @request['error']['reason']
+    end
+
+    # Return a code to error returning by janus
+    def code
+      @request['error']['code']
+    end
+  end
+end

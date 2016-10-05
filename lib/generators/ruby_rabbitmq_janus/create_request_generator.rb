7415bee0b76b01b37e82a3fb5b3c27d4a569e4f7
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-05 18:45:14 +0200

    Complete generator with creation file

--- lib/generators/ruby_rabbitmq_janus/create_request_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/create_request_generator.rb
@@ -5,9 +5,29 @@ module RubyRabbitmqJanus
     # Create an class for generate a custom configuration file
     class CreateRequestGenerator < Rails::Generators::Base
       desc 'Create an request to json format for RubyRabbitmqJanus transaction.'
+      argument :type_name, type: :string, default: ''
+      argument :janus_type, type: :string, default: ''
+      argument :content, type: :string, default: ''
 
       def create_request
-        puts "Create a super request in config/request/#{file_name}/#{class_name}.json"
+        File.open(file_json, 'w') do |file|
+          file.write(write_json)
+        end
+      end
+
+      private
+
+      def file_json
+        base_file = 'config/requests'
+        base_file = "#{base_file}/#{type_name}" unless type_name.empty?
+        "#{base_file}/#{janus_type}.json"
+      end
+
+      def write_json
+        hash = {}
+        hash['janus'] = janus_type
+        hash['body'] = content
+        JSON.pretty_generate(hash)
       end
     end
   end

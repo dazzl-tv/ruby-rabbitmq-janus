de6771b22581f92d44ee4b4dcce88d4baad4016c
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-06 11:05:10 +0200

    Update tool initializer

--- lib/generators/ruby_rabbitmq_janus/initializer_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/initializer_generator.rb
@@ -6,13 +6,10 @@ module RubyRabbitmqJanus
     class InitializerGenerator < Rails::Generators::Base
       desc 'Generate a initializer to this gem for rails application.'
 
-      def self.source_root
-        root_path = File.realpath(File.join(File.dirname(__FILE__), '..', '..', '..'))
-        @_rrj_source_root ||= File.join(root_path, 'config')
-      end
-
       def copy_initializer
-        template 'initializer.rb', 'config/initializers/ruby_rabbitmq_janus.rb'
+        initializer 'ruby_rabbitmq_janus.rb' do
+          "# frozen_string_literal: true\n\n@janus = RubyRabbitmqJanus::RRJ.new"
+        end
       end
     end
   end

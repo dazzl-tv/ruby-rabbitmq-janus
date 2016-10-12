6ff6c151dda9eb328f2bc08f0955126dc1476b0b
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-12 14:46:40 +0200

    Fix test rabbitmq

--- spec/rrj/rrj_rabbitmq_spec.rb
+++ spec/rrj/rrj_rabbitmq_spec.rb
@@ -2,15 +2,12 @@
 
 require 'spec_helper'
 
-describe 'RubyRabbitmqJanus::RabitMQ', type: :config do
-  before(:context) do
-    RubyRabbitmqJanus::Log.instance
-    RubyRabbitmqJanus::Config.instance
-    RubyRabbitmqJanus::Requests.instance
-    @rabbit = RubyRabbitmqJanus::RabbitMQ.new
+describe 'RubyRabbitmqJanus::RabitMQ', type: :config, name: :rabbit do
+  it 'Start connection with RabbitMQ server' do
+    expect(RubyRabbitmqJanus::Rabbit::Connect.new.start.class).to eq Bunny::Session
   end
 
-  it 'Close connection RabbitMQ Server' do
-    expect(@rabbit.send(:close_server_rabbitmq)).to eq :closed
+  it 'Close connection with RabbitMQ Server' do
+    expect(RubyRabbitmqJanus::Rabbit::Connect.new.close).to eq :closed
   end
 end

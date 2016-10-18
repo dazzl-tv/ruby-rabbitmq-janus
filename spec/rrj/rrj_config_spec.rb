e143617683ac211845935382112befa328d9dfac
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-18 13:26:49 +0200

    Fix modules calling

--- spec/rrj/rrj_config_spec.rb
+++ spec/rrj/rrj_config_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 describe 'RubyRabbitmqJanus::Config', type: :config, name: :config do
   it 'Configuration is corectly loading' do
-    expect(RubyRabbitmqJanus::Config.instance.options).to match_json_schema(:config)
+    expect(RubyRabbitmqJanus::Tools::Config.instance.options).to \
+      match_json_schema(:config)
   end
 end
--- spec/rrj/rrj_log_spec.rb
+++ spec/rrj/rrj_log_spec.rb
@@ -4,14 +4,14 @@ require 'spec_helper'
 
 describe 'RubyRabbitmqJanus::Log', type: :config, name: :log do
   it 'Log instance is correctly loading' do
-    expect(RubyRabbitmqJanus::Log.instance).not_to be nil
+    expect(RubyRabbitmqJanus::Tools::Log.instance).not_to be nil
   end
 
   it 'Default level log is DEBUG' do
-    expect(RubyRabbitmqJanus::Log.instance.level).to eq 0
+    expect(RubyRabbitmqJanus::Tools::Log.instance.level).to eq 0
   end
 
   it 'Default progname is RubyRabbitmqJanus' do
-    expect(RubyRabbitmqJanus::Log.instance.progname).to eq RubyRabbitmqJanus.name
+    expect(RubyRabbitmqJanus::Tools::Log.instance.progname).to eq RubyRabbitmqJanus.name
   end
 end

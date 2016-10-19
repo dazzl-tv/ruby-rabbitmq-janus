1bfab5b308bd47c177ef3fae2d566503bd1cba28
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-19 10:55:51 +0200

    Delete unless message logs

--- lib/rrj/tools/replaces.rb
+++ lib/rrj/tools/replaces.rb
@@ -9,7 +9,6 @@ module RubyRabbitmqJanus
       def initialize(request, options = {})
         @request = request
         @opts = options
-        Tools::Log.instance.debug "Option to replace in request #{@opts}"
       end
 
       # Replace element in hash request with information used for this transaction
@@ -35,52 +34,46 @@ module RubyRabbitmqJanus
 
       # Create an transaction string and replace in request field with an String format
       def replace_transaction
-        Tools::Log.instance.debug 'Replace transaction'
         @request['transaction'].replace [*('A'..'Z'), *('0'..'9')].sample(10).join
       rescue => message
-        Tools::Log.instance.debug "Error transaction replace : #{message}"
+        Tools::Log.instance.warning "Error transaction replace : #{message}"
       end
 
       # Read option session and replace in request
       def replace_session
-        Tools::Log.instance.debug 'Replace session'
         @request['session_id'] = @opts['session_id']
       rescue => message
-        Tools::Log.instance.debug "Error session replace : #{message}"
+        Tools::Log.instance.warning "Error session replace : #{message}"
       end
 
       # Replace plugin string
       def replace_plugin
-        Tools::Log.instance.debug 'Replace plugin'
         @request['plugin'] = Tools::Config.instance.options['janus']['plugins'][0]
       rescue => message
-        Tools::Log.instance.debug "Error plugin replace : #{message}"
+        Tools::Log.instance.warning "Error plugin replace : #{message}"
       end
 
       # Replace handle integer
       def replace_handle
-        Tools::Log.instance.debug 'Replace handle'
         @request['handle_id'] = @opts['handle_id']
       rescue => message
-        Tools::Log.instance.debug "Error handle replace : #{message}"
+        Tools::Log.instance.warning "Error handle replace : #{message}"
       end
 
       # Replace other element in request
       def replace_other
         values = @opts['replace']
-        Tools::Log.instance.debug "Replace other element : #{values}"
         running_hash(rewrite_key_to_string(values))
       rescue => message
-        Tools::Log.instance.debug "Error REPLACE other field : #{message}"
+        Tools::Log.instance.warning "Error REPLACE other field : #{message}"
       end
 
       # Adds other element to request
       def add_other
         values = @opts['add']
-        Tools::Log.instance.debug "Add other element : #{values}"
         @request['body'].merge!(values)
       rescue => message
-        Tools::Log.instance.debug "Error ADD other field : #{message}"
+        Tools::Log.instance.warning "Error ADD other field : #{message}"
       end
 
       # Rewrite key symbol to string

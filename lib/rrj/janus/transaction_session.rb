b8b9fa7e5d6609cc17593e587ad5ccbd83390603
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-10-27 18:08:20 +0200

    Transaction for simple session

---
+++ lib/rrj/janus/transaction_session.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module RubyRabbitmqJanus
+  module Janus
+    # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+    # This class work with janus and send a series of message
+    class TransactionSession < Transaction
+      # Connect to session and post an message
+      def session_connect(exclusive)
+        rabbit.transaction_short do
+          publish = choose_queue(exclusive)
+          Janus::Response.new(publish.send_a_message(yield)).to_json
+        end
+      end
+    end
+  end
+end

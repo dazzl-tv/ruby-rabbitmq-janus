840551c36dbb6c84f73d689b1f4bd703e958aea7
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-03 15:28:43 +0100

    Fix vetrsion increment

--- lib/rrj/info.rb
+++ lib/rrj/info.rb
@@ -3,7 +3,7 @@
 # Define constant to gem.
 module RubyRabbitmqJanus
   # Define version to gem
-  VERSION = '1.0.0'
+  VERSION = '1.0.1'
 
   # Define a summary description to gem
   SUMMARY = 'Ruby RabbitMQ Janus'

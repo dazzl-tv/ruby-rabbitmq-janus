08759f90d2f9b7adbdb872737b65f17f92cfa460
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-10 16:03:41 +0100

    Optimize event message reading

--- lib/rrj/janus/threads/event.rb
+++ lib/rrj/janus/threads/event.rb
@@ -10,42 +10,31 @@ module RubyRabbitmqJanus
       # Initialize Event object. Is used for listen an standard out queue to Janus
       def initialize
         Tools::Log.instance.debug 'Start listen events in from-janus queue'
+        @publish = @response = nil
         super
-        listen_live
+        start_thread
       end
 
-      # Start listen queue and return each message
+      # Start listen queue and work with each message reading
       def listen(&block)
-        initialize_thread(&block)
-        # @lock.synchronize { condition.wait(lock) }
-        # Tools::Log.instance.info "[x] Response : #{@response}"
-        # yield @response
+        loop { @publish.event_received(&block) }
       end
 
       private
 
-      # Listen q queue "from-janus"
-      def listen_live
-        # start_thread
-        initialize_thread(&block)
-      end
-
       # Intialize an listen to queue
-      def initialize_thread(&block)
-        start_transaction(&block)
+      def initialize_thread
+        start_transaction
       rescue Interrupt
         Tools::Log.instance.info 'Stop to listen standard queue'
         rabbit.close
       end
 
       # Start a transaction with Rabbit an Janus
-      def start_transaction(&block)
+      def start_transaction
         rabbit.transaction_long do
-          publisher = Rabbit::Publisher::Listener.new(rabbit)
-          publisher.listen_events
-          loop do
-            publisher.event_received(&block)
-          end
+          @publish = Rabbit::Publisher::Listener.new(rabbit)
+          @publish.listen_events
         end
       end
     end

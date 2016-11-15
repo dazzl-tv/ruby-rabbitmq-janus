334fdd957746a59693a703ad42ef9e6b7b40ca2e
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-15 16:15:15 +0100

    Create an threads for listen an execute block code

--- lib/rrj/janus/processus/event.rb
+++ lib/rrj/janus/processus/event.rb
@@ -7,43 +7,29 @@ module RubyRabbitmqJanus
       # For listen standard queue ("from-janus" by default)
       # :reek:InstanceVariableAssumption and :reek:NilCheck
       # :reek:TooManyInstanceVariables and :reek:TooManyStatements
-      class Event
+      class Event < Concurrency
         include Singleton
 
         # Initialize Event object. Is used for listen an standard out queue to Janus
         def initialize
+          super
           @publish = @response = nil
-          Tools::Log.instance.info "Create an thread -- #{self.class.name}"
-          @rabbit = Rabbit::Connect.new
-          @lock = Mutex.new
-          @condition = ConditionVariable.new
-          @thread_subscribe = Thread.new { initialize_thread }
         end
 
-        # Start listen queue and work with each message reading
-        def listen(&block)
-          @thread_subscribe.join
-          puts 'LISTEN !!'
-          @thread_listen = Thread.new do
-            puts 'LISTEN in thread !!'
-            Thread.pass
-            @publish.event_received(&block)
+        # Execute an block code in a thread
+        def run(&block)
+          @thread.join
+          Thread.new do
+            loop { @thread.thread_variable_get(:publish).listen_events(&block) }
           end
         end
 
         private
 
         # Initialize a thread
-        def initialize_thread
-          Thread.pass
-          @rabbit.transaction_long do
-            @publish = Rabbit::Publisher::Listener.new(@rabbit)
-            @response = @publish.listen_events
-            @thread_listen&.join # Call if not nil
-          end
-        rescue Interrupt
-          Tools::Log.instance.info "Stop transaction #{self.class.name}"
-          @rabbit.close
+        def transaction_running
+          Thread.current.thread_variable_set(:publish,
+                                             Rabbit::Publisher::Listener.new(rabbit))
         end
       end
     end
--- lib/rrj/rabbit/publish/listener.rb
+++ lib/rrj/rabbit/publish/listener.rb
@@ -15,19 +15,15 @@ module RubyRabbitmqJanus
           @rabbit = rabbit.channel
           @reply = @rabbit.queue(Tools::Config.instance.options['queues']['queue_from'])
           Tools::Log.instance.debug "Waiting message from queue : #{@reply.name}"
-        end
-
-        # Listen a queue and return a body response
-        def listen_events
           @reply.subscribe do |_info_delivery, _propertie, payload|
             synchronize_response(payload)
           end
         end
 
-        # Execute an action for each response received in queue
-        def event_received
-          loop do
-            return_response
+        # Listen a queue and return a body response
+        def listen_events
+          lock.synchronize do
+            condition.wait(lock)
             yield @response.event, @response.data, @response.jsep
           end
         end
@@ -38,7 +34,7 @@ module RubyRabbitmqJanus
         def synchronize_response(payload)
           @response = Janus::Response.new(JSON.parse(payload))
           Tools::Log.instance.info \
-            "[X] Message number reading : #{@count} --\n\r" \
+            "[X] Message number reading ##{@count} --\n\r" \
             "#{@response.to_hash}"
           @count += 1
           lock.synchronize { condition.signal }

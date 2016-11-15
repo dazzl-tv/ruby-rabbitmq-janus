1618b99483ecebb87e1f37c7f4c89fa7889568c4
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-15 16:14:42 +0100

    Fix test script listen queue

--- listen.rb
+++ listen.rb
@@ -4,7 +4,7 @@
 require 'ruby_rabbitmq_janus'
 
 @t = RubyRabbitmqJanus::RRJ.new
-@events = RubyRabbitmqJanus::Janus::Concurrencies::Event.instance
+@e = RubyRabbitmqJanus::Janus::Concurrencies::Event.instance
 
 # :reek:NilCheck and :reek:TooManyStatements
 def case_event(data, jsep)
@@ -13,9 +13,6 @@ def case_event(data, jsep)
   when 'joined'
     puts 'Joined request ...'
     @t.handle_message_simple('channel::offer', jsep)
-  when 'selected' then puts 'Selected request ...'
-  when 'unselected' then puts 'Unselected request ...'
-  when 'left' then puts 'Left request ...'
   end
   update_jsep(jsep) unless jsep.nil?
 end
@@ -24,33 +21,25 @@ def update_jsep(jsep)
   puts "JSEP : #{jsep}"
 end
 
-def case_hangup
-  puts 'REASON : Hangup'
-  Thread.current.stop
-end
-
 def case_stop
   puts 'REASON : Stop'
   Thread.current.stop
 end
 
-def case_error
-  puts 'REASON : Error ...'
-end
-
-puts '## Start listen Block'
-@events.listen do |reason, data, jsep|
+events = lambda do |reason, data = nil, jsep = nil|
+  puts "Execute block code with reason : #{reason}"
   case reason
   when 'event' then case_event(data, jsep)
-  when 'hangup' then case_hangup
   when 'stop' then case_stop
-  when 'error' then case_error
   else
     puts 'REASON default'
   end
 end
+
+puts '## Start listen Block'
+@e.run(&events)
 puts '## End listen block'
 
-puts 'apps running'
+puts '## APPS RUNNING'
 loop do
 end

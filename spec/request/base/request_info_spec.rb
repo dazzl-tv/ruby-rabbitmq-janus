915f38487c2ba943cdebf138778da22e01c1c3b3
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-16 17:22:37 +0100

    Complete test with exclusive/non exclusive queue

--- spec/request/base/request_info_spec.rb
+++ spec/request/base/request_info_spec.rb
@@ -2,13 +2,25 @@
 
 require 'spec_helper'
 
-describe 'RubyRabbitmqJanus::RRJ' do
-  describe '.response', type: :request, level: :base, name: :info do
-    let(:type) { 'base::info' }
-    let(:message) { @gateway.message_simple(type) }
+describe 'RubyRabbitmqJanus::RRJ -- message type info' do
+  # Configure requests test before sending request
+  before(:example) do
+    @gateway = RubyRabbitmqJanus::RRJ.new
+  end
 
-    it 'type info' do
-      expect(message.to_json).to match_json_schema(type)
+  describe '#message_simple', type: :request, level: :base, name: :info do
+    let(:type) { 'base::info' }
+
+    context 'when queue is exclusive' do
+      it do
+        expect(@gateway.message_simple(type, true).to_json).to match_json_schema(type)
+      end
+    end
+
+    context 'when queue is not exclusive' do
+      it 'should match JSON empty' do
+        expect(@gateway.message_simple(type).to_json).to eq('{}')
+      end
     end
   end
 end

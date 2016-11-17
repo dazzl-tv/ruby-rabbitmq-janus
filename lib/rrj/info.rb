8fcd3d5a5ded3147c587e9dd1ca9c6b1b51b84db
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-17 16:06:39 +0100

    Increment version

--- lib/rrj/info.rb
+++ lib/rrj/info.rb
@@ -3,7 +3,7 @@
 # Define constant to gem.
 module RubyRabbitmqJanus
   # Define version to gem
-  VERSION = '1.0.7'
+  VERSION = '1.0.8'
 
   # Define a summary description to gem
   SUMMARY = 'Ruby RabbitMQ Janus'

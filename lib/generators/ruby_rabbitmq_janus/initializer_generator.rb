9a8170e56ca8986c85bf7f980f573caf16a496ab
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-25 11:53:56 +0100

    Uupdate generator intializer

--- lib/generators/ruby_rabbitmq_janus/initializer_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/initializer_generator.rb
@@ -9,9 +9,32 @@ module RubyRabbitmqJanus
       # Create an initializer
       def copy_initializer
         initializer 'ruby_rabbitmq_janus.rb' do
-          "# frozen_string_literal: true\n\n" \
-            "::RRJ = RubyRabbitmqJanus::RRJ.new\n"\
-            '::JanusEvents = RubyRabbitmqJanus::Janus::Concurrencies::Event.instance'
+          <<-INIT
+# frozen_string_literal: true
+
+# This test disable this gems execution when you running an task with rake
+unless File.basename($PROGRAM_NAME) == 'rake'
+  # Write your methods for listening standar queue here
+  #
+  # Example :
+  # action_events = lambda do |reason, data|
+  # logger.debug 'Execute block code with reason :'
+  # logger.debug reason
+  # logger.debug --
+  # logger.debug data
+  # end
+
+  Rails.configuration.after_initialize do
+    # Initialize a gem and create an session with a keepalive
+    ::RRJ = RubyRabbitmqJanus::RRJ.new
+
+    # If you want listen a standard queue and execute an block code uncomment
+    # this line and craete an method sendind to run
+    # ::JEvents = RubyRabbitmqJanus::Janus::Concurrencies::Event.instance
+    # JEvents.run(&action_events)
+  end
+end
+          INIT
         end
       end
     end

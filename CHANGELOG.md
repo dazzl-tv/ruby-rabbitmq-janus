3d749f968cce9a3471b56c16201434bb82d692f9
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-11-29 10:58:36 +0100

    Increment version and complete changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+
+## [1.1.8] - 2016-11-29
+### Changed
+- Return handle created with start_handle method
+- Complete info in README.md
+- Change metrics length
+### Deleted
+- Delete a test script `listen.rb`
+
 ## [1.1.7] - 2016-11-28
 ### Changed
 - Use handle if is given in arguments for handle_message_simple
--- lib/rrj/info.rb
+++ lib/rrj/info.rb
@@ -3,17 +3,19 @@
 # Define constant to gem.
 module RubyRabbitmqJanus
   # Define version to gem
-  VERSION = '1.1.7'
+  VERSION = '1.1.8'
 
   # Define a summary description to gem
   SUMMARY = 'Ruby RabbitMQ Janus'
 
   # Define a long description to gem
-  DESCRIPTION = 'This gem is used to communicate to a server Janus through RabbitMQ '\
-    'software (Message-oriented middleware). It waiting a messages to Rails API who ' \
-    'send to RabbitMQ server in a queue for janus server. Janus processes a message ' \
-    'and send to RabbitMQ server in a queue for gem. Once the received message is ' \
-    'decoded and returned through the Rails API.'
+  DESCRIPTION = <<-DESC
+This gem is used to communicate to a server Janus through RabbitMQ software
+(Message-oriented middleware). It waiting a messages to Rails API who send to
+RabbitMQ server in a queue for janus server. Janus processes a message and send
+to RabbitMQ server in a queue for gem. Once the received message is decoded and
+returned through the Rails API.
+    DESC
 
   # Define homepage
   HOMEPAGE = 'https://github.com/dazzl-tv/ruby-rabbitmq-janus'

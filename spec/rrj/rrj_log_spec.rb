91f090185f2f2ec37c348829766926a2f37dfc52
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-12-01 17:31:05 +0100

    Change default level

--- spec/rrj/rrj_log_spec.rb
+++ spec/rrj/rrj_log_spec.rb
@@ -8,10 +8,14 @@ describe 'RubyRabbitmqJanus::Log', type: :config, name: :log do
   end
 
   it 'Default level log is DEBUG' do
-    expect(RubyRabbitmqJanus::Tools::Log.instance.level).to eq 0
+    # 0 = debug
+    # ...
+    # 5 = unknown
+    expect(RubyRabbitmqJanus::Tools::Log.instance.level).to eq 1
   end
 
   it 'Default progname is RubyRabbitmqJanus' do
-    expect(RubyRabbitmqJanus::Tools::Log.instance.progname).to eq RubyRabbitmqJanus.name
+    expect(RubyRabbitmqJanus::Tools::Log.instance.progname).to \
+      eq RubyRabbitmqJanus.name
   end
 end

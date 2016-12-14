4c922b89444f1ed9843f1b76e7e7cbab2159feb1
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-12-14 12:33:30 +0100

    Complete documentation

--- lib/rrj/init.rb
+++ lib/rrj/init.rb
@@ -10,23 +10,35 @@ require 'key_path'
 require 'active_support'
 require 'erb'
 
+# Primary module for this gem
 module RubyRabbitmqJanus
   # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+
+  # # RubyRabbitmqJanus - RRJ
+  #
   # Initialize RRJ gem and create automatically a session with janus and
   # sending a keepalive message. The Time To Live is configured in yaml
-  # configuration file 'config/default.yml' or
-  # 'config/ruby-rabbitmq-janus.yml'.
+  # configuration file `config/default.yml` or if you a customize config in
+  # `config/ruby-rabbitmq-janus.yml`.
+  #
   # @!attribute [r] session
-  #   @return [Fixnum] Return an session number created in instantiate this gem.
-  # :reek:BooleanParameter and :reek:ControlParameter and :reek:DataClump
-  # :reek:LongParameterList and :reek:TooManyStatements
+  #   @return [Fixnum] Return an session number created when this gem is
+  #     instanciate. It's janus who creates the number of the session.
+  #
+  # @see file:/config/requests.md For more information to type requests used.
+  # @see file:/config/default.md For more information to config file used.
   class RRJ
     attr_reader :session
 
-    # Returns a new instance of RubyRabbitmqJanus
+    # Return a new instance of RubyRabbitmqJanus.
+    #
+    # @example Create a instance to this gem
+    #   @rrj = RubyRabbitmqJanus::RRJ.new
+    #   => #<RubyRabbitmqJanus::RRJ:0x007 @session=123, @transaction=nil>
     def initialize
-      # Instanciate singleton tools
-      start_instances_tools
+      Tools::Log.instance
+      Tools::Config.instance
+      Tools::Requests.instance
 
       # Create an session while time opening
       @session = Janus::Concurrencies::Keepalive.instance.session
@@ -35,19 +47,25 @@ module RubyRabbitmqJanus
       @transaction = nil
     end
 
-    # Send an simple message to janus. No options in request with this method.
+    # Send an simple message to janus.
+    #
     # @param [String] type
     #   Given a type to request. JSON request writing in 'config/requests/'
     # @param [Bollean] exclusive
     #   Use an exclusive queue or not
+    # @param [Hash] options Options update in request
+    #
     # @example Sending an message info in queue 'to-janus'
     #   RubyRabbitmqJanus::RRJ.new.message_simple('base::info', true)
     #   #=> {}
     # @example Sending an message info in queue exclusive 'ampq.gen-xxxxx'
     #   RubyRabbitmqJanus::RRJ.new.message_simple('base::info')
     #   #=> {"janus":"server_info","name":"Janus WebRTC Gateway" ... }
+    #
     # @return [RubyRabbitmqJanus::Janus::Responses::Standard]
     #   Give an object response to janus server
+    #
+    # @since 1.0.0
     def message_simple(type = 'base::info', exclusive = false, options = {})
       Janus::Transactions::Transaction.new(@session).connect(exclusive) do
         Janus::Messages::Standard.new(type, options)
@@ -55,16 +73,21 @@ module RubyRabbitmqJanus
     end
 
     # Send an message simple in current session.
+    #
     # @param [String] type
     #   Given a type to request. JSON request writing in 'config/requests/'
     # @param [Hash] options Options update in request
     # @param [Bollean] exclusive
     #   Use an exclusive queue or not
+    #
     # @example Sending an message create
     #   RubyRabbitmqJanus::RRJ.new.message_session('base::create')
-    #   #=> {"janus":"server_info","name":"Janus WebRTC Gateway" ... }
+    #   n#=> {"janus":"server_info","name":"Janus WebRTC Gateway" ... }
+    #
     # @return [RubyRabbitmqJanus::Janus::Responses::Standard]
-    # Give an object response to janus server
+    #   Give an object response to janus server
+    #
+    # @since 1.0.0
     def message_session(type, options = {}, exclusive = true)
       Janus::Transactions::Session.new(@session).session_connect(exclusive) do
         Janus::Messages::Standard.new(type, use_current_session?(options))
@@ -73,15 +96,21 @@ module RubyRabbitmqJanus
       raise Errors::RRJErrorPost, error
     end
 
-    # Send a message simple for admin Janus
+    # Send a message simple for admin Janus.
+    # **Is used just for sending a message to Janus Monitor/Admin API**.
+    #
     # @param [String] type
-    #   Given a type to request. JSON request writing in 'config/requests/'
+    #   Given a type to request. JSON request writing in `config/requests/`
     # @param [Hash] options Options update in request
+    #
     # @example Sending an message create
     #   RubyRabbitmqJanus::RRJ.new.message_admin('admin::sessions')
     #   #=> {"janus":"success","sessions": [12345, 8786567465465, ...] }
+    #
     # @return [RubyRabbitmqJanus::Janus::Responses::Standard]
-    # Give an object response to janus server
+    #   Give an object response to janus server
+    #
+    # @since 1.0.0
     def message_admin(type, options = {})
       Janus::Transactions::Admin.new(use_current_session?(options)).connect do
         Janus::Messages::Admin.new(type, options)
@@ -89,21 +118,40 @@ module RubyRabbitmqJanus
     end
 
     # Send an message in handle session in current session.
+    #
     # @param [String] type
     #   Given a type to request. JSON request writing in 'config/requests/'
     # @param [Hash] replace Options update in request
     # @param [Hash] add Elements adding to request
+    #
     # @example Sending an message create
     #   RubyRabbitmqJanus::RRJ.new.message_session('base::create')
     #   #=> {"janus":"server_info","name":"Janus WebRTC Gateway" ... }
+    #
     # @return [RubyRabbitmqJanus::Janus::Responses::Standard]
-    # Give an object response to janus server
+    #   Give a object response to janus server
+    #
+    # @since 1.0.0
     def message_handle(type, replace = {}, add = {})
       options = { 'replace' => replace, 'add' => add }
       @transaction.publish_message_handle(type, options)
     end
 
     # Define an handle transaction and establish connection with janus
+    #
+    # @param [Bollean] exclusive
+    #   Use an exclusive queue or not
+    #
+    # @yield Sending requests to Janus.
+    #
+    # @example Start a transaction with janus with a exclusive queue
+    #   @rrj.start_handle(true) do
+    #     @rrj.message_handle('peer:trickle')
+    #   end
+    #
+    # @see #message_handle
+    #
+    # @since 1.0.0
     def start_handle(exclusive = false)
       @transaction = Janus::Transactions::Handle.new(@session)
       @transaction.handle_connect(exclusive) { yield }
@@ -111,41 +159,34 @@ module RubyRabbitmqJanus
       raise Errors::RRJErrorHandle, error
     end
 
-    # Define an handle admin transaction and establish connection with janus
-    def start_handle_admin
-      @transaction = Janus::Transactions::Admin.new(@session)
-      @transaction.handle_connect { yield }
-    rescue => error
-      raise Errors::RRJErrorHandle, error
-    end
-
-    # Stop an handle existing in session running
-    def stop_handle
-      @transaction.handle_running_stop
-    end
-
-    # Start an short transaction. Create an handle and send a message to queue.
-    # This queue is not exclusive, so this message is sending in queue
-    # 'to-janus' and a response is return in queue 'from-janus'
+    # Start an short transaction. Create an handle and send one message to
+    # queue. **This queue is not exclusive**, so this message is sending in
+    # queue 'to-janus' and a response is return in queue 'from-janus'.
+    #
+    # @param [String] type
+    #   Given a type to request. JSON request writing in 'config/requests/'
+    # @param [Hash] options Fields updating in request sending to janus.
+    #   **This hash must imperatively contains the key `replace`**
+    # @option options [Hash] :replace Contains all fields who be replaced in
+    #   request sending to janus.
+    # @option options [Hash] :add Contains all fields adding to request.
+    #
     # @example Send an request trickle
-    #   candidate = {
+    #   cdn = {
     #     'candidate' => {
     #       'sdpMid' => 'video',
     #       'sdpMLineIndex' => 1,
     #       'candidate' => '...'
     #     }
     #   }
-    #   RubyRabbitmqJanus::RRJ.new.handle_message_simple('peer::trickle',
-    #                                                    candidate)
-    #   #=> { 'janus' => 'trickle', ..., 'candidate' => {
-    #     'completed' : true
-    #     }
-    #   }
-    def handle_message_simple(type, replace = {}, add = {}, exclusive = false)
-      handle = replace.include?('handle_id') ? replace['handle_id'] : 0
+    #   RubyRabbitmqJanus::RRJ.new.handle_message_simple('peer::trickle', cdn)
+    #   #=> { 'janus' => 'trickle', ..., 'candidate' => { ... } }
+    #
+    # @since 1.0.0
+    def handle_message_simple(type, options = { replace: {}, add: {} })
       @transaction = Janus::Transactions::Handle.new(@session)
-      @transaction.handle_connect_and_stop(exclusive, handle) do
-        message_handle(type, replace, add)
+      @transaction.handle_connect_and_stop(handle?(options)) do
+        message_handle(type, options[:replace], options[:add])
       end
     rescue => error
       raise Errors::RRJErrorHandle, error
@@ -155,16 +196,12 @@ module RubyRabbitmqJanus
 
     attr_accessor :transaction
 
-    # Start singleton instances
-    def start_instances_tools
-      Tools::Log.instance
-      Tools::Config.instance
-      Tools::Requests.instance
-    end
-
-    # Return a current session if not specified
     def use_current_session?(option)
       { 'session_id' => @session } unless option.key?('session_id')
+    end
+
+    def handle?(option)
+      option[:replace].include?('handle_id') ? replace['handle_id'] : 0
     end
   end
 end

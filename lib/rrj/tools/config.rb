ed0a66af109bcf831e14e20cc655de5663d87d79
Jérémy Vaillant <jeremy.vaillant@dazzl.tv>
2016-12-14 13:01:43 +0100

    Complete documentation

--- lib/rrj/tools/config.rb
+++ lib/rrj/tools/config.rb
@@ -3,13 +3,17 @@
 module RubyRabbitmqJanus
   module Tools
     # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
-    # Loading a yaml file for apply a configuration to gem.
+
+    # Singletong object for reading configuration file
+    #
+    # @!attribute [r] options
+    #   @return [Hash] Return all options to configured in config file.
     class Config
       include Singleton
 
       attr_reader :options
 
-      # Define HOME RRJ
+      # Define HOME RRJ gem
       RRJ_HOME = File.realpath(File.join(File.dirname(__FILE__),
                                          '..', '..', '..'))
 
@@ -19,7 +23,7 @@ module RubyRabbitmqJanus
       # Define a default override file configuration
       CONF_CUSTOM = 'config/ruby-rabbitmq-janus.yml'
 
-      # Define a default path to file configuration
+      # Define a default path to file configuration to gem
       PATH_DEFAULT = File.join(RRJ_HOME, CONF_DEFAULT)
 
       # Initialize configuration file default or customize if exist
@@ -27,34 +31,38 @@ module RubyRabbitmqJanus
         @options = nil
         conf_customize
         conf_default
-        define_log_level_used
+        Tools::Log.instance.save_level
       end
 
-      # Read configuration queue from
+      # @return [String] read configuration for queue `from`
       def queue_from
         @options['queues']['standard']['from']
       end
 
-      # Read configuration queue to
+      # @return [String] read configuration for queue `to`
       def queue_to
         @options['queues']['standard']['to']
       end
 
-      # Read configuration queue admin from
+      # @return [String] read configuration for queue admin `from`
       def queue_admin_from
         @options['queues']['admin']['from']
       end
 
-      # Read configuration queue admin to
+      # @return [String] read configuration for queue admin `to`
       def queue_admin_to
         @options['queues']['admin']['to']
       end
 
+      # @return [Symbol] read configuration for log lvel used in this gem
+      def log_level
+        @options['gem']['log']['level'].upcase.to_sym
+      rescue
+        raise Errors::LevelNotDefine
+      end
+
       private
 
-      # Load configuration file yaml
-      # @return [Yaml] Configuration file
-      # @param file [String] Path to configuration file (with name)
       def load_configuration(file)
         Tools::Log.instance.info("Loading configuration file : #{file}")
         YAML.load(ERB.new(File.read(file)).result)
@@ -62,24 +70,14 @@ module RubyRabbitmqJanus
         raise Errors::ConfigFileNotFound, file
       end
 
-      # Load customize configuration file if exist
       def conf_customize
         file = File.join(Dir.pwd, CONF_CUSTOM)
         @options = load_configuration(file) if File.exist?(file)
       end
 
-      # Load default configuration if customize configuration doesn't exist
       def conf_default
         file = PATH_DEFAULT
         @options ||= load_configuration(file)
-      end
-
-      # Define log lvel used in this gem
-      def define_log_level_used
-        Tools::Log.instance.level = \
-          Tools::Log::LEVELS[@options['gem']['log']['level'].upcase.to_sym]
-      rescue
-        raise Errors::LevelNotDefine
       end
     end
   end

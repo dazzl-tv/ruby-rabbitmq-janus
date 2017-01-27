e4d17f8f70ab2295fecf61d6a5739146c8299472
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-01-27 16:16:55 +0100

    Optimize session management

--- lib/rrj/janus/processus/keepalive.rb
+++ lib/rrj/janus/processus/keepalive.rb
@@ -5,31 +5,39 @@ module RubyRabbitmqJanus
     module Concurrencies
       # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
 
-      # # Manage sending keepalive message
+      # # Manage keepalive message
       #
-      # Create a thread for sending keepalive to session created by this
-      # instanciate gem
+      # Create a thread for sending a message with type keepalive to session
+      # created by this instanciate gem
+      #
+      # @see https://ruby-doc.org/stdlib-2.4.0/libdoc/singleton/rdoc/Singleton.html
       class Keepalive < Concurrency
         include Singleton
 
-        # Create a thread
+        # Initialize a singleton object for sending keepalive to janus
         def initialize
-          @response = @publisher = nil
+          @pub = @session = nil
           super
         end
 
+        # Give a session Integer created when this gem is intanciate.
+        # Is waiting a thread return a response to message created sending.
+        #
+        # @example Ask session
+        #   Keepalive.instance.session
+        #   => 852803383803249
         # @return [Fixnum] Identifier to session created by Janus
         def session
           lock.synchronize do
             condition.wait(lock)
-            running_session
+            @session
           end
         end
 
         private
 
         def transaction_running
-          @response = Janus::Responses::Standard.new(create_session)
+          @session = find_session
           synchronize
           create_loop_session
         rescue => error
@@ -46,29 +54,26 @@ module RubyRabbitmqJanus
 
         def loop_session(time_to_live)
           sleep time_to_live
-          @publisher.publish(message_keepalive)
-          Tools::Log.instance.info "Keepalive for #{running_session}"
+          @pub.publish(message_keepalive)
+          Tools::Log.instance.info "Keepalive for #{@session}"
         end
 
         def create_session
-          @publisher = Rabbit::Publisher::PublishExclusive.new(rabbit.channel,
-                                                               '')
-          @publisher.publish(Janus::Messages::Standard.new('base::create'))
+          @pub = Rabbit::Publisher::PublishExclusive.new(rabbit.channel, '')
+          @pub.publish(Janus::Messages::Standard.new('base::create'))
         rescue => error
           raise Errors::KeepaliveCreateSession, error
         end
 
-        def running_session
-          @response.session
-        rescue => error
-          raise Errors::KeepaliveSessionReturn, error
-        end
-
         def message_keepalive
-          opt = { 'session_id' => running_session }
+          opt = { 'session_id' => @session }
           Janus::Messages::Standard.new('base::keepalive', opt)
         rescue => error
           raise Errors::KeepaliveMessage, error
+        end
+
+        def find_session
+          Janus::Responses::Standard.new(create_session).to_hash['data']['id']
         end
       end
     end

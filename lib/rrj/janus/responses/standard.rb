2337a18c862581177cecc24e196d70fe5cb06d66
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-02-06 15:38:09 +0100

    Add method for simplify response data

--- lib/rrj/janus/responses/standard.rb
+++ lib/rrj/janus/responses/standard.rb
@@ -16,11 +16,31 @@ module RubyRabbitmqJanus
           data_id
         end
 
+        # Read response for plugin request
+        def plugin
+          request['plugindata']
+        end
+
+        # Read data response for plugin request
+        def plugin_data
+          plugin['data']
+        end
+
+        # Read data response for normal request
+        def data
+          request['data']
+        end
+
+        # Read SDP response
+        def sdp
+          request['jsep']['sdp']
+        end
+
         private
 
         # Read a hash and return an identifier
         def data_id
-          request['data']['id'].to_i
+          data['id'].to_i
         rescue => error
           raise Errors::JanusResponseDataId, error
         end

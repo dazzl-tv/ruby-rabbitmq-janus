5d06471669c32c41923d931d42b2d191e2dea5c6
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-02-09 16:18:05 +0100

    Complete doc and add handle_id test

--- lib/rrj/tools/gem/option.rb
+++ lib/rrj/tools/gem/option.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# :reek:FeatureEnvy :reek:UtilityFunction
+
 module RubyRabbitmqJanus
   module Tools
     # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
@@ -9,30 +11,46 @@ module RubyRabbitmqJanus
     # This class start all singleton, Log, Config, Request and Keepalice
     # instance. It's alos used for testing session/handle used in request.
     class Option
-      attr_reader :hash
-
       def initialize
         Log.instance
         Config.instance
         Requests.instance
         @session = Janus::Concurrencies::Keepalive.instance.session
-        @hash = {}
       end
 
+      # Give number session to keepalive
+      #
+      # @return [Fixnum] Session ID
+      #
+      # @since 2.0.0
       def keepalive
         @session
       end
 
+      # Determine session_id used
+      #
+      # @param [Hash] options Read options used in request
+      #
+      # @return [Fixnum] Session ID
+      #
+      # @since 2.0.0
       def use_current_session?(options)
-        @hash = options
-        @hash['session_id'] = @session if session_exist?
-        @hash['session_id']
+        hash = options
+        hash['session_id'] = @session unless hash.key?('session_id')
+        hash['session_id']
       end
 
-      private
-
-      def session_exist?
-        !@hash.key?('session_id')
+      # Determine handle_id used
+      #
+      # @param [Hash] options Read options used in request
+      #
+      # @return [Fixnum] Handle ID
+      #
+      # @since 2.0.0
+      def use_current_handle?(options)
+        hash = options
+        hash['handle_id'] = 0 unless hash.key?('handle_id')
+        hash['handle_id']
       end
     end
   end

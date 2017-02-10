826bf16d4e02f7e04528bb7726dbca5d0b6b4d82
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-02-10 11:01:11 +0100

    Add constructor

--- lib/rrj/janus/transactions/session.rb
+++ lib/rrj/janus/transactions/session.rb
@@ -8,18 +8,38 @@ module RubyRabbitmqJanus
       # # Manage a transaction
       # Manage a transaction with message if contains a session identifier
       class Session < Transaction
+        # Initialize a transaction with handle
+        #
+        # @param [Fixnum] session
+        #   Use a session identifier for created message
+        def initialize(exclusive, session)
+          super(session)
+          @exclusive = exclusive
+        end
+
         # Opening a short transaction with rabbitmq and close when is ending
         #
         # @param [Boolean] exclusive
         #   Determine if the message is sending to a exclusive queue or not
         #
         # @yield Send a message to Janus
-        def connect(exclusive)
-          @exclusive = exclusive
+        def connect
           rabbit.transaction_short do
             choose_queue
-            send_a_message { yield }
+            yield
           end
+        end
+
+        def publish_message(type, options = {})
+          msg = Janus::Messages::Standard.new(type, opts.merge!(options))
+          response = read_response(publisher.publish(msg))
+          Janus::Responses::Standard.new(response)
+        end
+
+        private
+
+        def opts
+          { 'session_id' => session }
         end
       end
     end

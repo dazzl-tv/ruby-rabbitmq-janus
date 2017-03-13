c01433853ce29f6dfe32b0465a1753988e6077fa
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-03-13 16:20:47 +0100

    Update deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,4 +2,4 @@
 
 gem install gems -v 0.8.3
 gem install dpl -v 1.8.27
-dpl --provider=rubygems --api-key=$RUBY_GEMS_API_KEY --gemspec=./rrj.gemspec
+dpl --provider=rubygems --api-key=$RUBY_GEMS_API_KEY --gemspec=../rrj.gemspec

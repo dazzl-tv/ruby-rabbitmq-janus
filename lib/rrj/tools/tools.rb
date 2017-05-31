637369b83a883085bfeb091565439d233518478d
travis@localhost <travis@localhost>
2017-05-31 19:03:10 +0200

    Fix require file

--- lib/rrj/tools/tools.rb
+++ lib/rrj/tools/tools.rb
@@ -5,6 +5,7 @@ require 'rrj/tools/gem/config'
 require 'rrj/tools/gem/requests'
 require 'rrj/tools/gem/cluster'
 require 'rrj/tools/gem/option'
+require 'rrj/tools/gem/janus_instance'
 require 'rrj/tools/replaces/type'
 require 'rrj/tools/replaces/replace'
 

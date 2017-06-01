7f2d80d0ff5b2694cd74aed8ab9f87ed290923c3
travis@localhost <travis@localhost>
2017-06-01 13:11:07 +0200

    Configure generator for create migration with Active Record [skip ci]

--- lib/generators/ruby_rabbitmq_janus/migration_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/migration_generator.rb
@@ -1,21 +1,24 @@
 # frozen_string-literal: true
 
-require 'rails/generators/active_record'
+if defined?(ActiveRecord)
+  require 'rails/generators/active_record'
 
-module RubyrabbitmqJanus
-  module Generators
-    class MigrationGenerator < ::Rails::Generators::Base
-      include Rails::Generators::Migration
-      source_root File.expand_path('../templates', __FILE__)
-      desc 'Installs RubyRabbitmqJanus migration file.'
+  module RubyrabbitmqJanus
+    module Generators
+      class MigrationGenerator < ::Rails::Generators::Base
+        include Rails::Generators::Migration
+        desc 'Installs RubyRabbitmqJanus migration file.'
 
-      def install
-        migration_template 'migration.rb',
-                           'db/migrate/create_ruby_rabbitmq_janus_tables.rb'
-      end
+        source_root File.expand_path('../templates', __FILE__)
 
-      def self.next_migration_number(dirname)
-        ActiveRecord::Generators::Base.next_migration_number(dirname)
+        def install
+          migration_template 'migration.rb',
+                             'db/migrate/create_ruby_rabbitmq_janus_tables.rb'
+        end
+
+        def self.next_migration_number(dirname)
+          ActiveRecord::Generators::Base.next_migration_number(dirname)
+        end
       end
     end
   end

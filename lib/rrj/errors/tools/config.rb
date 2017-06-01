1f334b9e8ee1c1de767acc25a0ac30f7c72027e3
travis@localhost <travis@localhost>
2017-06-01 15:56:18 +0200

    Complete error definition for Config class

--- lib/rrj/errors/tools/config.rb
+++ lib/rrj/errors/tools/config.rb
@@ -7,36 +7,82 @@ module RubyRabbitmqJanus
     # Define a super class for all error in class Config
     class Config < RRJError
       # Initalize a error for Config class
-      def initialize(message, level)
+      def initialize(message, level = :fatal)
         msg = "[Config] #{message} -- #{Tools::Log.instance.configuration}"
         super(msg, level)
       end
     end
 
-    # Define an error if the configuration file is not here
-    class FileNotFound < Config
-      # Initialize a error for Config class if file don't exist.
-      # It's a fatal error
-      def initialize
-        super 'Error for configuration file, does on exist.', :fatal
+    module Config
+      # Error for Config#initializer
+      class Initializer < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error in initalizer'
+        end
       end
-    end
 
-    # Define and error if rubrik level is not present
-    class LevelNotDefine < Config
-      # Initialize a error for config class if log level option is not present.
-      # It's a warning error
-      def initialize
-        super \
-          'Error in configuration file : option level is not present.', :fatal
+      # Error for Config#queue_from
+      class QueueFrom < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading standard queue from'
+        end
       end
-    end
 
-    # Define a error if janus/session/keepalive is not present
-    class TTLNotFound < Config
-      # Initialize a message error and level
-      def initialize
-        super 'Keepalive TTL option is not present in config file.', :fatal
+      # Error for Config#queue_to
+      class QueueTo < RubyuRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading standard queue to'
+        end
+      end
+
+      # Error for config#queue_admin_from
+      class QueueAdminFrom < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading admin queue from'
+        end
+      end
+
+      # Error for Config#queue_admin_to
+      class QueueAdminTo < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading admin queue to'
+        end
+      end
+
+      # Error for Config#log_level
+      class LevelNotDefine < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading option level', :warn
+        end
+      end
+
+      # Error for Config#time_to_live (or #ttl)
+      class TTLNotFound < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Keepalive TTL option is not reading in config file', :warn
+        end
+      end
+
+      # Error for Config#plugin_at
+      class PluginAt < RubyRabbitmqJanus::Errors::Config
+        def initialize(parameter)
+          super "Plugin is not found in configuration file, " \
+                "with parameter #{parameter}", :warn
+        end
+      end
+
+      # Error for Config#cluster
+      class Cluster < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading cluster enabled option', :fatal
+        end
+      end
+
+      # Error for Config#number_of_instance
+      class NumberOfinstance < RubyRabbitmqJanus::Errors::Config
+        def initialize
+          super 'Error for reading cluster instance count', :fatal
+        end
       end
     end
   end
--- lib/rrj/tools/gem/config.rb
+++ lib/rrj/tools/gem/config.rb
@@ -35,56 +35,85 @@ module RubyRabbitmqJanus
         loading_configuration_customize
         loading_configuration_default
         Tools::Log.instance.save_level(log_level)
+      rescue
+        raise Errors::Config::Initializer
       end
 
+      # Get to name queue_from (pattern)
+      #
       # @return [String] read configuration for queue `from`
       def queue_from
         @options['queues']['standard']['from'].to_s
+      rescue
+        raise Errors::Config::QueueFrom
       end
 
+      # Get to name queue_to (pattern)
+      #
       # @return [String] read configuration for queue `to`
       def queue_to
-        Tools::Cluster.instance.queue_to
+        @options['queues']['standard']['to'].to_s
+      rescue
+        raise Errors::Config::QueueTo
       end
 
+      # Get to name queue_admin_from (pattern)
+      #
       # @return [String] read configuration for queue admin `from`
       def queue_admin_from
         @options['queues']['admin']['from'].to_s
+      rescue
+        raise Errors::Config::QueueAdminFrom
       end
 
+      # Get to name queue_admin_from (pattern)
+      #
       # @return [String] read configuration for queue admin `to`
       def queue_admin_to
         @options['queues']['admin']['to'].to_s
+      rescue
+        raise Errors::Config::QueueAdminTo
       end
 
       # @return [Symbol] read configuration for log level used in this gem
       def log_level
         @options['gem']['log']['level'].upcase.to_sym
       rescue
-        raise Errors::LevelNotDefine, @configuration
+        raise Errors::Config::LevelNotDefine
       end
 
       # @return [Integer]
       #   read configuration for janus time to live for keepalive messages
       def time_to_live
-        @options['janus']['session']['keepalive'].to_i
+        @options['janus']['session']['keepalive'].to_i || 50
       rescue
-        raise Errors::TTLNotFound, @configuration
+        raise Errors::Config::TTLNotFound
       end
+
+      alias ttl time_to_live
 
       # @param [Fixnum] index determine what field is readint in array plugins
       #   in configuration file
       # @return [String] read configuration for plugin with index
       def plugin_at(index = 0)
         @options['janus']['plugins'][index].to_s
+      rescue
+        raise Errors::Config::PluginAt, index
       end
 
       # @return [Boolean] Read option file for a janus cluster section
-      def cluster?
+      def cluster
         @options['janus']['cluster']['enabled'].to_s.eql?('true') ? true : false
+      rescue
+        raise Errors::Config::Cluster
       end
 
-      alias ttl time_to_live
+      # Count number of Janus instances
+      def number_of_instance
+        @options['janus']['cluster']['count'].to_i || 1
+      rescue
+        raise Errors::Config::NumberOfInstance
+      end
 
       private
 
@@ -92,8 +121,6 @@ module RubyRabbitmqJanus
         log_message = "Loading configuration file : #{@configuration}"
         Tools::Log.instance.info(log_message)
         YAML.safe_load(ERB.new(File.read(@configuration)).result)
-      rescue
-        raise Errors::FileNotFound, @configuration
       end
 
       def loading_configuration_customize

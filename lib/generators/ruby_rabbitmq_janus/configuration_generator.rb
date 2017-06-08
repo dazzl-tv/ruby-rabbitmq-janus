d9dff92baa1900808df5d00e8f2643781249fd2a
travis@localhost <travis@localhost>
2017-06-08 13:32:06 +0200

    Cleaning code

--- lib/generators/ruby_rabbitmq_janus/configuration_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/configuration_generator.rb
@@ -8,21 +8,11 @@ module RubyRabbitmqJanus
       desc 'Generate a custom configuration file.'
 
       # Define root path for original file a copied
-      def self.source_root
-        root_path = File.realpath(root_path_gem)
-        @_rrj_source_root ||= File.join(root_path, 'config')
-      end
+      source_root File.expand_path('../../../config', __dir__)
 
       # Copy a default configuration in config folder to Rails app
       def copy_configuration
         template 'default.yml', 'config/ruby-rabbitmq-janus.yml'
-      end
-
-      private
-
-      # Define a root path
-      def root_path_gem
-        File.join(File.dirname(__FILE__), '..', '..', '..')
       end
     end
   end

a88ae01aa640192c8f5cfab21ada43ef16f847b4
travis@localhost <travis@localhost>
2017-06-08 13:27:54 +0200

    Cleaning code

--- lib/generators/ruby_rabbitmq_janus/default_request_generator.rb
+++ lib/generators/ruby_rabbitmq_janus/default_request_generator.rb
@@ -7,21 +7,11 @@ module RubyRabbitmqJanus
       desc 'Copy base request file sending to janus in application.'
 
       # Define root path for original file a copied
-      def source_root
-        root_path = File.realpath(root_path_gem)
-        @_rrj_source_root ||= File.join(root_path, 'config')
-      end
+      source_root File.expand_path('../../../config', __dir__)
 
       # Copy default request in Rails app
       def copy_default_request
         directory 'requests', 'config/requests'
-      end
-
-      private
-
-      # Define a root path
-      def root_path_gem
-        File.join(File.dirname(__FILE__), '..', '..', '..')
       end
     end
   end

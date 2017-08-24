d22ea30bb692e1f990107e63f5ec037c3c76c01e
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-08-24 15:11:51 +0200

    Fix ID name if mongo or active record

--- spec/rrj/models/janus_instance.rb
+++ spec/rrj/models/janus_instance.rb
@@ -7,7 +7,7 @@ describe RubyRabbitmqJanus::Models::JanusInstance, type: :model,
   let(:model) { RubyRabbitmqJanus::Models::JanusInstance }
 
   context 'active record model' do
-    it { expect(model.attribute_names).to include('_id') }
+    it { expect(model.attribute_names).to include(ENV['MONGO'].match?('true') ? '_id' : 'id') }
     it { expect(model.attribute_names).to include('instance') }
     it { expect(model.attribute_names).to include('session') }
     it { expect(model.attribute_names).to include('enable') }

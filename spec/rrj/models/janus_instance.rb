7a3b335ba17803a53e42a9173c2f91fd71b3a9d2
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-08-24 15:06:40 +0200

    Use model mongo or active record with ENV

--- spec/rrj/models/janus_instance.rb
+++ spec/rrj/models/janus_instance.rb
@@ -1,28 +1,15 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/ConstantName
-
 require 'spec_helper'
 
 describe RubyRabbitmqJanus::Models::JanusInstance, type: :model,
                                                    name: :janus_instance do
-  before do
-    ActiveRecord::Core = nil if defined?(ActiveRecord)
-    require 'mongoid' unless defined?(Mongoid)
-  end
+  let(:model) { RubyRabbitmqJanus::Models::JanusInstance }
 
-  context 'mongoid model' do
-    let(:model) { RubyRabbitmqJanus::Models::JanusInstance }
-
+  context 'active record model' do
     it { expect(model.attribute_names).to include('_id') }
     it { expect(model.attribute_names).to include('instance') }
     it { expect(model.attribute_names).to include('session') }
     it { expect(model.attribute_names).to include('enable') }
   end
-
-  after do
-    Object.send(:remove_const, :Mongoid) if defined?(Mongoid)
-    require 'active_record' unless defined?(ActiveRecord)
-  end
 end
-# rubocop:enable Style/ConstantName

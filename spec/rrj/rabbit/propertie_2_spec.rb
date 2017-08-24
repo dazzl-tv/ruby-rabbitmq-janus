840987be4b58e2337eb62c4576d90371ca0d8da4
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2017-08-24 17:00:00 +0200

    Refractoring test create message for janus instance 2

--- spec/rrj/rabbit/propertie_2_spec.rb
+++ spec/rrj/rabbit/propertie_2_spec.rb
@@ -3,26 +3,25 @@
 require 'spec_helper'
 
 describe RubyRabbitmqJanus::Rabbit::Propertie, type: :rabbit,
-                                               name: :propertie,
-                                               instances: true do
-  let(:rabbit) { RubyRabbitmqJanus::Rabbit::Propertie.new(2) }
+                                               name: :propertie do
+  let(:rabbit) { RubyRabbitmqJanus::Rabbit::Propertie.new }
 
   describe '#options' do
-    it { expect(rabbit.options).to match_json_schema(:rabbit_options2) }
+    it { expect(rabbit.options).to match_json_schema(:rabbit_options) }
   end
 
   describe '#options_admin' do
     context 'For admin request' do
       it do
         expect(rabbit.options_admin('admin::sessions')).to \
-          match_json_schema(:rabbit_options_admin2)
+          match_json_schema(:rabbit_options_admin)
       end
     end
 
     context 'For base request' do
       it do
         expect(rabbit.options_admin('request::create')).to \
-          match_json_schema(:rabbit_options2)
+          match_json_schema(:rabbit_options)
       end
     end
   end

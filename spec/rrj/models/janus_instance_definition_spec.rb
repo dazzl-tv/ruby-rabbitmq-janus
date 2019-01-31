16e0a503bfd73efe23418d3bc35147b98d1332b5
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-01-31 16:14:43 +0100

    Fix model fields for SQlite

--- spec/rrj/models/janus_instance_definition_spec.rb
+++ spec/rrj/models/janus_instance_definition_spec.rb
@@ -5,24 +5,25 @@ require 'spec_helper'
 describe RubyRabbitmqJanus::Models::JanusInstance, type: :model,
                                                    name: :janus_instance do
   let(:model) { RubyRabbitmqJanus::Models::JanusInstance }
+  let(:parameter) do
+    {
+      'id' => '_id',
+      'session_id' => 'session',
+      'thread_id' => 'thread',
+      'thread_id_adm' => 'thread_adm',
+      'instance' => '_id'
+    }
+  end
 
   context 'Janus Instance model definition' do
     if ENV['MONGO'].match?('true')
       it { expect(model.attribute_names).to include('_id') }
       it do
-        parameter = {
-          'id' => '_id',
-          'session_id' => 'session',
-          'thread_id' => 'thread',
-          'thread_id_adm' => 'thread_adm',
-          'instance' => '_id'
-        }
         expect(model.aliased_fields).to eq(parameter)
       end
     else
       it { expect(model.attribute_names).to include('id') }
       it do
-        parameter = { 'instance' => 'id', 'session_id' => 'session', 'thread_id' => 'thread' }
         expect(model.attribute_aliases).to eq(parameter)
       end
     end

a3985f80fbe733034249605186283f13fd386bf9
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-01-31 10:45:45 +0100

    Add listener admin

---
+++ lib/rrj/rabbit/publish/listener_admin.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module RubyRabbitmqJanus
+  module Rabbit
+    module Publisher
+      class ListenerAdmin < Listener
+        private
+
+        def subscribe_queue
+          reply = @rabbit.queue(Tools::Config.instance.queue_admin_from)
+          @rabbit.prefetch(1)
+          reply.bind(binding).subscribe(opts_subs) do |info, prop, payload|
+            Tools::Log.instance.info \
+              "[X] Message ADMIN reading ##{prop['correlation_id']}"
+            synchronize_response(info, payload)
+          end
+        end
+      end
+    end
+  end
+end

198acb95f8ed10bc4278c9c1de491db0674ebfef
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-10-17 13:04:19 +0200

    Remove block comment

--- lib/rrj/tools/bin/init.rb
+++ lib/rrj/tools/bin/init.rb
@@ -7,23 +7,22 @@ require 'rrj/rails' # defined?(::Rails::Engine)
 # require File.join(File.dirname(__FILE__), '..', '..', 'binary')
 
 begin
-=begin
-  bin = RubyRabbitmqJanus::Binary.new
-  Log.info \
-    'Prepare to listen events in queue : ' + \
-    RubyRabbitmqJanus::Tools::Config.instance.queue_janus_instance
-  rabbit = RubyRabbitmqJanus::Rabbit::Connect.new
-  rabbit.start
-  listener = RubyRabbitmqJanus::Rabbit::Listener::JanusInstance.new(rabbit)
-  Log.info 'Loop events provided by Janus queues'
-  loop do
-    listener.listen_events do |event, response|
-      Log.debug "Event : #{event}"
-      Log.debug "Response : #{response.to_hash}"
-      bin.update_instance(response.to_hash)
-    end
-  end
-=end
+  # bin = RubyRabbitmqJanus::Binary.new
+  # Log.info \
+  #   'Prepare to listen events in queue : ' + \
+  #   RubyRabbitmqJanus::Tools::Config.instance.queue_janus_instance
+  # rabbit = RubyRabbitmqJanus::Rabbit::Connect.new
+  # rabbit.start
+  # listener = RubyRabbitmqJanus::Rabbit::Listener::JanusInstance.new(rabbit)
+  # Log.info 'Loop events provided by Janus queues'
+  # loop do
+  #   listener.listen_events do |event, response|
+  #     Log.debug "Event : #{event}"
+  #     Log.debug "Response : #{response.to_hash}"
+  #     bin.update_instance(response.to_hash)
+  #   end
+  # end
+
   Log.info 'Listen Public queue to Janus'
   loop {}
 rescue => exception

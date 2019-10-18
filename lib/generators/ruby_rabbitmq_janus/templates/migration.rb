161790acc5a87c6c413b0898eb65c4e94b294ad9
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-10-18 15:04:51 +0200

    Delete thread field

--- lib/generators/ruby_rabbitmq_janus/templates/migration.rb
+++ lib/generators/ruby_rabbitmq_janus/templates/migration.rb
@@ -4,12 +4,13 @@
 
 # Fields for JanusInstance model
 class CreateRubyRabbitmqJanusTables < ActiveRecord::Migration[5.0]
+  # Change for model JanusInstance
   def change
     create_table :janus_instances, id: false, force: true do |table|
       table.string :id, null: false
+      table.string :name
       table.integer :session, limit: 8
       table.boolean :enable
-      table.integer :thread, limit: 8
     end
   end
 end

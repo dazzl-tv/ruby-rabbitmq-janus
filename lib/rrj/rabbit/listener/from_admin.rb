e6be0be9affe154ee15a15205aad71a18674bf2f
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-10-18 11:01:42 +0200

    Refactor listeners

--- lib/rrj/rabbit/listener/from_admin.rb
+++ lib/rrj/rabbit/listener/from_admin.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
-# :reek:InstanceVariableAssumption
-
 module RubyRabbitmqJanus
   module Rabbit
     module Listener
-      # Listener to admin queue
+      # @author VAILLANT Jeremy <jeremy.vaillant@dazzl.tv>
+      #
+      # This publisher don't post message. Is listen just an admin queue to
+      # Janus. By default is "from-janus-admin". It's a parameter in config
+      # to this gem.
       class FromAdmin < From
         private
 
+        def reply
+          rabbit.queue(Tools::Config.instance.queue_admin_from)
+        end
+
         def subscribe_queue
-          reply = rabbit.queue(Tools::Config.instance.queue_admin_from)
           rabbit.prefetch(1)
           reply.bind(binding).subscribe(opts_subs) do |info, prop, payload|
             info_subscribe(info, prop, payload)

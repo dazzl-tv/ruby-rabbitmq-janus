4c8c953ee3b05f8f70c43767a3e8b033eaed0a3c
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-11-28 12:25:35 +0100

    Fix rule factory class name

--- spec/factories/janus_instance.rb
+++ spec/factories/janus_instance.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :janus_instance, class: RubyRabbitmqJanus::Models::JanusInstance do
+  factory :janus_instance, class: 'RubyRabbitmqJanus::Models::JanusInstance' do
     enable { true }
   end
 end

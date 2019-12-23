3154908414ba6198bd9fa52bbe2e1fe3677f42b4
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2019-12-23 12:08:28 +0100

    Add missing field to model

--- spec/factories/janus_instance.rb
+++ spec/factories/janus_instance.rb
@@ -3,5 +3,7 @@
 FactoryBot.define do
   factory :janus_instance, class: 'RubyRabbitmqJanus::Models::JanusInstance' do
     enable { true }
+    session_id { (rand * 10_000_000_000).to_i }
+    name { [*('a'..'z'), *('0'..'9')].sample(12).join }
   end
 end
